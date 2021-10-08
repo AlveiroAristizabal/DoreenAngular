929d402d3f94efd426a34706f5849c65764cf535
jaabalv <alvinstrumentos@hotmail.com>
2021-10-08 09:04:10 -0500

    dosDoreen

--- dist/assets/alb.js
+++ dist/assets/alb.js
@@ -1,240 +1,126 @@
-(function($) {
-    "use strict";
-
-    $(window).on('load', function() {
-        $(".preloader").fadeOut("slow", function() {
-            $(".preloader-left").addClass("slide-left");
-        });
-
-        $('#lionhero').owlCarousel({
-            animateOut: 'fadeOut',
-            nav: true,
-            navText: [
-                '<i class="ion-ios-arrow-thin-left"></i>',
-                '<i class="ion-ios-arrow-thin-right"></i>'
-            ],
-            items: 1,
-            navSpeed: 400,
-            loop: true,
-            autoplay: true,
-            autoplayTimeout: 4000,
-            autoplayHoverPause: true,
-        });
-
-        $('#liontextslider').owlCarousel({
-            nav: false,
-            items: 1,
-            navSpeed: 400,
-            loop: true,
-            autoplay: true,
-            autoplayTimeout: 4000,
-            autoplayHoverPause: true,
-        });
-
-        $('#liontestimonial').owlCarousel({
-            nav: true,
-            navText: [
-                '<i class="ion-ios-arrow-thin-left"></i>',
-                '<i class="ion-ios-arrow-thin-right"></i>'
-            ],
-            items: 1,
-            navSpeed: 400,
-            loop: true,
-            autoplay: true,
-            autoplayTimeout: 4000,
-            autoplayHoverPause: true,
-        });
-    });
-
-    $('.portfolio-block, .menu-item').on('click', function() {
-
-        //Portfolio masonry
-        var $container = $('#portfolio-container');
-        $container.isotope({
-            masonry: {
-                columnWidth: '.portfolio-item'
-            },
-            itemSelector: '.portfolio-item'
-        });
-        $('#filters').on('click', 'li', function() {
-            $('#filters li').removeClass('active');
-            $(this).addClass('active');
-            var filterValue = $(this).attr('data-filter');
-            $container.isotope({ filter: filterValue });
-        });
-
-    });
-    // alv modifica
-    // $('#filters').on('click', 'li', function() {
-    //     $('#filters li').removeClass('active');
-    //     $(this).addClass('active');
-    //     var filterValue = $(this).attr('data-filter');
-    //     $container.isotope({ filter: filterValue });
-    // });
-
-    // Typing Animation (Typed.js)
-    $('#element').typed({
-        strings: ["UX, UI Designer", "Web App Developer", "Social Animal!"],
-        typeSpeed: -50,
-        loop: true,
-        startDelay: 500,
-        backDelay: 3000,
-        contentType: 'html',
-    });
-
-    //Video background
-    var myPlayer;
-    $(function() {
-        myPlayer = $("#bgndVideo").YTPlayer({ useOnMobile: true, mobileFallbackImage: "assets/mask-4.png" });
-
-    });
-
-    //Portfolio Modal
-    $(document).on('click', '.open-project', function() {
-        var projectUrl = $(this).attr("href");
-        $('.inline-menu-container').removeClass('showx');
-        $('.sidebar-menu').addClass('hidex');
-        $('.content-blocks.pop').addClass('showx');
-        $('.content-blocks.pop section').load(projectUrl + ' .load-data > *');
-        return false;
-    });
-
-    //Blog post Modal
-    $('.open-post').on('click', function() {
-        var postUrl = $(this).attr("href");
-        $('.inline-menu-container').removeClass('showx');
-        $('.sidebar-menu').addClass('hidex');
-        $('.content-blocks.pop').addClass('showx');
-        $('.content-blocks.pop section').load(postUrl);
-        return false;
-    });
-
-    //On Click Open Menu Items
-    $('.menu-block, .menu-item').on('click', function() {
-        $('.name-block').addClass('reverse');
-        $('.name-block-container').addClass('reverse');
-        $('.menu-blocks').addClass('hidex');
-        $('.inline-menu-container').addClass('showx');
-        $('.inline-menu-container.style2').addClass('dark');
-    });
-    //On Click Open About/Resume Block
-    $('.about-block, .menu-item.about').on('click', function() {
-        $('.content-blocks').removeClass('showx');
-        $('.content-blocks.about').addClass('showx');
-        $('.menu-item').removeClass('active');
-        $('.menu-item.about').addClass('active');
-    });
-    //On Click Open Portfolio Block
-    $('.portfolio-block, .menu-item.portfolio').on('click', function() {
-        $('.content-blocks').removeClass('showx');
-        $('.content-blocks.portfolio').addClass('showx');
-        $('.menu-item').removeClass('active');
-        $('.menu-item.portfolio').addClass('active');
-    });
-    // alv cambia  ini
-    //On Click Open Portfolio Block  ALV ALV
-    // function a() {
-    //     $('.content-blocks').removeClass('showx');
-    //     $('.content-blocks.portfolio').addClass('showx');
-    //     $('.menu-item').removeClass('active');
-    //     $('.menu-item.portfolio').addClass('active');
-    // };
-
-    // alv cambia
-
-    //On Click Open Blog Block
-    $('.blog-block, .menu-item.blog').on('click', function() {
-        $('.content-blocks').removeClass('showx');
-        $('.content-blocks.blog').addClass('showx');
-        $('.menu-item').removeClass('active');
-        $('.menu-item.blog').addClass('active');
-    });
-    //On Click Open Contact Block
-    $('.contact-block, .menu-item.contact').on('click', function() {
-        $('.content-blocks').removeClass('showx');
-        $('.content-blocks.contact').addClass('showx');
-        $('.menu-item').removeClass('active');
-        $('.menu-item.contact').addClass('active');
-    });
-
-    //On Click Close Blocks
-    $('#close').on('click', function() {
-        $('.name-block').removeClass('reverse');
-        $('.name-block-container').removeClass('reverse');
-        $('.content-blocks').removeClass('showx');
-        $('.menu-blocks').removeClass('hidex');
-        $('.inline-menu-container').removeClass('showx');
-        $('.menu-item').removeClass('active');
-    });
-    //On Click Close Blog Post And Project Details
-    $('#close-pop').on('click', function() {
-        $('.content-blocks.pop').removeClass('showx');
-        $('.sidebar-menu').removeClass('hidex');
-        $('.inline-menu-container').addClass('showx');
-        $('.content-blocks.pop section').empty();
-    });
-
-    $('.menu-block, .menu-item, #close').on('click', function() {
-        $('.content-blocks').animate({ scrollTop: 0 }, 800);
-    });
-
-    //Function for 'Index-Menu2.html'
-    $('#home').on('click', function() {
-        $('.content-blocks').removeClass('showx');
-        $('.menu-item').removeClass('active');
-        $(this).addClass('active');
-        $('.inline-menu-container.style2').removeClass('dark');
-    });
-
-    //Custom Cursor
-    if (('.cursor').length > 0) {
-        var $circleCursor = $('.cursor');
-
-        function moveCursor(e) {
-            var t = e.clientX + "px",
-                n = e.clientY + "px";
-            TweenMax.to($circleCursor, .2, {
-                left: t,
-                top: n,
-                ease: 'Power1.easeOut'
-            });
+angular.module('ui.bootstrap.demo').controller('ModalDemoCtrl', function ($uibModal, $log, $document) {
+    var $ctrl = this;
+    $ctrl.items = ['item1', 'item2', 'item3'];
+  
+    $ctrl.animationsEnabled = true;
+  
+    $ctrl.open = function (size, parentSelector) {
+      var parentElem = parentSelector ? 
+        angular.element($document[0].querySelector('.modal-demo ' + parentSelector)) : undefined;
+      var modalInstance = $uibModal.open({
+        animation: $ctrl.animationsEnabled,
+        ariaLabelledBy: 'modal-title',
+        ariaDescribedBy: 'modal-body',
+        templateUrl: 'myModalContent.html',
+        controller: 'ModalInstanceCtrl',
+        controllerAs: '$ctrl',
+        size: size,
+        appendTo: parentElem,
+        resolve: {
+          items: function () {
+            return $ctrl.items;
+          }
         }
-        $(window).on('mousemove', moveCursor);
-
-        function zoomCursor() {
-            TweenMax.to($circleCursor, .2, {
-                borderWidth: '1px',
-                opacity: 0.1,
-                scale: 2,
-                ease: 'Power1.easeOut'
-            });
+      });
+  
+      modalInstance.result.then(function (selectedItem) {
+        $ctrl.selected = selectedItem;
+      }, function () {
+        $log.info('Modal dismissed at: ' + new Date());
+      });
+    };
+  
+    $ctrl.openComponentModal = function () {
+      var modalInstance = $uibModal.open({
+        animation: $ctrl.animationsEnabled,
+        component: 'modalComponent',
+        resolve: {
+          items: function () {
+            return $ctrl.items;
+          }
         }
-
-        $(document).on('mouseenter', 'a, .zoom-cursor, .menu-item', zoomCursor);
-
-        function noCursor() {
-            TweenMax.to($circleCursor, .2, {
-                opacity: 0,
-                ease: 'Power1.easeOut'
-            });
+      });
+  
+      modalInstance.result.then(function (selectedItem) {
+        $ctrl.selected = selectedItem;
+      }, function () {
+        $log.info('modal-component dismissed at: ' + new Date());
+      });
+    };
+  
+    $ctrl.openMultipleModals = function () {
+      $uibModal.open({
+        animation: $ctrl.animationsEnabled,
+        ariaLabelledBy: 'modal-title-bottom',
+        ariaDescribedBy: 'modal-body-bottom',
+        templateUrl: 'stackedModal.html',
+        size: 'sm',
+        controller: function($scope) {
+          $scope.name = 'bottom';  
         }
-        $(document).on('mouseenter', 'button, .btn', noCursor);
-
-        function defaultCursor() {
-            TweenLite.to($circleCursor, .1, {
-                borderWidth: '2px',
-                opacity: 0.5,
-                scale: 1,
-                ease: 'Power1.easeOut'
-            });
+      });
+  
+      $uibModal.open({
+        animation: $ctrl.animationsEnabled,
+        ariaLabelledBy: 'modal-title-top',
+        ariaDescribedBy: 'modal-body-top',
+        templateUrl: 'stackedModal.html',
+        size: 'sm',
+        controller: function($scope) {
+          $scope.name = 'top';  
         }
-
-        $(document).on('mouseleave', 'a, .zoom-cursor, .menu-item, button, .btn', defaultCursor);
-
-        $(document).ready(function() {
-
-            $('.cursor').css('transform', 'scale(1)');
-        });
+      });
+    };
+  
+    $ctrl.toggleAnimation = function () {
+      $ctrl.animationsEnabled = !$ctrl.animationsEnabled;
+    };
+  });
+  
+  // Please note that $uibModalInstance represents a modal window (instance) dependency.
+  // It is not the same as the $uibModal service used above.
+  
+  angular.module('ui.bootstrap.demo').controller('ModalInstanceCtrl', function ($uibModalInstance, items) {
+    var $ctrl = this;
+    $ctrl.items = items;
+    $ctrl.selected = {
+      item: $ctrl.items[0]
+    };
+  
+    $ctrl.ok = function () {
+      $uibModalInstance.close($ctrl.selected.item);
+    };
+  
+    $ctrl.cancel = function () {
+      $uibModalInstance.dismiss('cancel');
+    };
+  });
+  
+  // Please note that the close and dismiss bindings are from $uibModalInstance.
+  
+  angular.module('ui.bootstrap.demo').component('modalComponent', {
+    templateUrl: 'myModalContent.html',
+    bindings: {
+      resolve: '<',
+      close: '&',
+      dismiss: '&'
+    },
+    controller: function () {
+      var $ctrl = this;
+  
+      $ctrl.$onInit = function () {
+        $ctrl.items = $ctrl.resolve.items;
+        $ctrl.selected = {
+          item: $ctrl.items[0]
+        };
+      };
+  
+      $ctrl.ok = function () {
+        $ctrl.close({$value: $ctrl.selected.item});
+      };
+  
+      $ctrl.cancel = function () {
+        $ctrl.dismiss({$value: 'cancel'});
+      };
     }
-
-})(jQuery);
+  });
